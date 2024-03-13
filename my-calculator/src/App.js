b16aa35606814f8e79b90c8e2ad68c4802023bf3
EunJi Kim <dust9629@gmail.com>
2024-03-13 23:59:26 +0900

    Feat : add frame

--- my-calculator/src/App.js
+++ my-calculator/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import React, { useState } from 'react';
+import './App.css'
 
 function App() {
+  // const [value, setValue] = useState('')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='calculator'>
+        <form action=''>
+          <div>
+            <input type='text' />
+          </div>
+          <div>
+            <input type='button' value="AC" />
+            <input type='button' value="DE" />
+            <input type='button' value="." />
+            <input type='button' value="/" />
+          </div>
+          <div>
+            <input type='button' value="7" />
+            <input type='button' value="8" />
+            <input type='button' value="9" />
+            <input type='button' value="*" />
+          </div>
+          <div>
+            <input type='button' value="4" />
+            <input type='button' value="5" />
+            <input type='button' value="6" />
+            <input type='button' value="+" />
+          </div>
+          <div>
+            <input type='button' value="1" />
+            <input type='button' value="2" />
+            <input type='button' value="3" />
+            <input type='button' value="-" />
+          </div>
+          <div>
+            <input type='button' value="00" />
+            <input type='button' value="0" />
+            <input type='button' value="=" />
+          </div>
+        </form>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
